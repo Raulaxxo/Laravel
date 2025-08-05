64002ee6396c74b911463c39ab15fcfd904ce6d8
Raul Silva <raulaxxo@gmail.com>
2025-08-05 01:52:17 -0400

    composer update en el contenedor antes de las migraciones

--- migraciones.sh
+++ migraciones.sh
@@ -3,6 +3,10 @@
 # Nombre del contenedor PHP
 CONTAINER=laravel-app
 
+echo "📦 Ejecutando Composer Update dentro del contenedor $CONTAINER..."
+
+docker exec -it $CONTAINER composer update
+
 echo "📦 Ejecutando migraciones en $CONTAINER..."
 
 docker exec -it $CONTAINER php artisan migrate --force
