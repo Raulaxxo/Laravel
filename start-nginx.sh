efb61e75885ca07e487b43e98ff9e3c21dc06eb4
Raul Silva <raulaxxo@gmail.com>
2025-08-05 14:27:55 -0400

    cambio script virtualhost

--- start-nginx.sh
+++ start-nginx.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
-envsubst '${APP_DOMAIN}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+
+# Verifica que APP_DOMAIN esté definido
+if [ -z "$APP_DOMAIN" ]; then
+  echo "ERROR: APP_DOMAIN no está definido"
+  exit 1
+fi
+
+# Define nombre del archivo final
+TARGET_CONF="/etc/nginx/conf.d/${APP_DOMAIN}.conf"
+
+# Sustituye variable en template y guarda con el nombre del dominio
+envsubst '${APP_DOMAIN}' < /etc/nginx/conf.d/default.conf.template > "$TARGET_CONF"
+
+# Inicia nginx
 nginx -g 'daemon off;'
