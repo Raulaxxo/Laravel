5eb829271103f20f47407454cc75136529d5edc3
Raul Silva <raulaxxo@gmail.com>
2025-08-04 23:28:51 -0400

    healtcheck

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Ruta para healthcheck (devuelve JSON con status OK)
+Route::get('/health', function () {
+    return response()->json(['status' => 'OK'], 200);
+});
